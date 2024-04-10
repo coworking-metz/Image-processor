d84b2ad8b2471092d5e9315d3a291c893e7bd849
Coworking Metz <contact@coworking-metz.fr>
2024-04-10 11:36:37 +0200

    vs hit

--- lib/utils.inc.php
+++ lib/utils.inc.php
@@ -1,12 +1,25 @@
 <?php
 
 
-function cloudflareHit($url=false) {
-    if(!$url) {
-        $url = (empty($_SERVER['HTTPS']) ? 'http' : 'https') . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";    
+function cloudflareHit(array $urls) {
+    if(!$urls) return;
+    
+    if(!is_array($urls)) {
+        $urls = [$urls];
     }
-    file_get_contents('https://cloudflare.coworking-metz.fr/hit?url='.urlencode($url));
+
+    $data = ['urls' => $urls];
+    $options = [
+        'http' => [
+            'header'  => "Content-type: application/json\r\n",
+            'method'  => 'POST',
+            'content' => json_encode($data),
+        ],
+    ];
+    $context = stream_context_create($options);
+    file_get_contents('https://cloudflare.coworking-metz.fr/hit', false, $context);
 }
+
 function noCacheHeaders() {
     header("Cache-Control: no-cache, must-revalidate, max-age=0");
     header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
